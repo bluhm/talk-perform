9e02dda67d2e333ab3b301dd463e87f83813ef7d
Alexander Bluhm <alexander.bluhm@gmx.net>
2020-02-01 22:20:56 +0100

    Get gnuplot.pl in line with regress-all.

--- gnuplot.pl
+++ gnuplot.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 # collect cvs logs between certain dates for sub branches
 
-# Copyright (c) 2018 Alexander Bluhm <bluhm@genua.de>
+# Copyright (c) 2018-2020 Alexander Bluhm <bluhm@genua.de>
 # Copyright (c) 2018-2019 Moritz Buhl <mbuhl@genua.de>
 #
 # Permission to use, copy, modify, and distribute this software for any
@@ -32,28 +32,31 @@ my $scriptname = "$0 @ARGV";
 my %opts;
 getopts('vnC:D:N:T:X:x:Y:y:', \%opts) or do {
     print STDERR <<"EOF";
-usage: $0 [-vn] [-D date] [-N number] -T tcp|make|udp|fs
+usage: $0 [-vn] [-D date] [-N number] -T test
 [-x min] [X max] [-y min] [Y max] new_file.tex
     -v		verbose
     -n		dry run
     -D date	run date
     -N number	test number
-    -T test	test name (tcp, make, upd, fs)
-    -x min	x range minimum 
+    -T test	test name (tcp|tcp6|linux|linux6|make|udp|udp6|fs)
+    -x min	x range minimum
     -X max	x range maximum
-    -y min	y range minimum 
+    -y min	y range minimum
     -Y max	y range maximum
 EOF
     exit(2);
 };
 my $verbose = $opts{v};
 my $dry = $opts{n};
-my $run = str2time($opts{D})
-    or die "Invalid -D date '$opts{D}'"
-    if ($opts{D});
+my ($run, $date);
+if ($opts{D}) {
+    $run = str2time($opts{D})
+	or die "Invalid -D date '$opts{D}'";
+    $date = $opts{D};
+}
 my $tstnum = $opts{N};
 my $test = $opts{T}
-    or die "Option -T tcp|make|udp|fs missing";
+    or die "Option -T test missing";
 my ($xmin, $xmax, $ymin, $ymax);
 if (defined $opts{x}) {
     $opts{x} =~ /^\d+$/
@@ -96,33 +99,32 @@ open (my $fh, '<', $testdata)
     or die "Open '$testdata' for reading failed: $!";
 
 <$fh>; # skip file head
-my ($tst, $sub, undef, undef, undef, undef, $unit)  = split(/\s+/, <$fh>);
+my ($tst, $sub, undef, undef, undef, undef, $unit)  = split(" ", <$fh>);
 $tests{"$tst $sub"} = 1;
-
-while (my $row = <$fh>) {
-    my ($tst, $sub) = split(/\s+/, $row);
+while (<$fh>) {
+    ($tst, $sub) = split;
     $tests{"$tst $sub"} = 1;
 }
 
-my @testarr = sort keys %tests;
-my $testnames = join(" ", @testarr);
-$testnames = $testarr[$tstnum] if $tstnum;
+my @tests = sort keys %tests;
+@tests = $tests[$tstnum] if $tstnum;
 my %q = quirks();
-my $quirks = join(" ", sort keys %q);
+my @quirks = sort keys %q;
 
-my @plotvars = ("DATA_FILE='$testdata'",
-"OUT_FILE='$out'",
-"QUIRKS='$quirks'",
-"TESTS='$testnames'",
-"TITLE='$title'",
-"UNIT='$unit'");
-my @plotcmd = ("gnuplot", "-d");
-
+my @plotvars = (
+    "DATA_FILE='$testdata'",
+    "OUT_FILE='$out'",
+    "QUIRKS='@quirks'",
+    "TESTS='@tests'",
+    "TITLE='$title'",
+    "UNIT='$unit'"
+);
 push @plotvars, "RUN_DATE='$run'" if $run;
 push @plotvars, "XRANGE_MIN='$xmin'" if defined $xmin;
 push @plotvars, "XRANGE_MAX='$xmax'" if defined $xmax;
 push @plotvars, "YRANGE_MIN='$ymin'" if defined $ymin;
 push @plotvars, "YRANGE_MAX='$ymax'" if defined $ymax;
+my @plotcmd = ("gnuplot", "-d");
 if ($dry) {
     push @plotcmd, (map { ("-e", "\"$_\"") } @plotvars);
     push @plotcmd, $plotfile;
